32da86d9d13101297e6624ec9a97cbbfbce2d510
Ygor Cruz <ygorcruzfelix@gmail.com>
2019-08-07 14:03:36 -0300

    Continuada aula 2

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
+const DevController = require('./controllers/DevController')
+const LikeController = require('./controllers/LikeController')
+
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.json({ message: `Hello World ${req.query.name}` })
-})
+routes.post('/devs', DevController.store)
+routes.post('/devs/:devId/likes', LikeController.store)
 
 module.exports = routes
