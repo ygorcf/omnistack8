89f6419c4c025d0a71cb1af7cbfc9e883719fb79
Ygor Cruz <ygorcruzfelix@gmail.com>
2019-08-10 21:31:47 -0300

    Continuando a aula de react

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,64 @@
 import React from 'react'
 
-export default function Main() {
-  return <h1>mainha</h1>
+import './Main.css'
+import logo from '../assets/logo.svg'
+import like from '../assets/like.svg'
+import dislike from '../assets/dislike.svg'
+
+export default function Main({ match }) {
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev"/>
+      <ul>
+        <li>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/1200px-Flag_of_Brazil.svg.png" alt=""/>
+          <footer>
+            <strong>Felipe</strong>
+            <p>Descricao de teste por enquanto</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/1200px-Flag_of_Brazil.svg.png" alt=""/>
+          <footer>
+            <strong>Felipe</strong>
+            <p>Descricao de teste por enquanto</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/1200px-Flag_of_Brazil.svg.png" alt=""/>
+          <footer>
+            <strong>Felipe</strong>
+            <p>Descricao de teste por enquanto</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
 }
